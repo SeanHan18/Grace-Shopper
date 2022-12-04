de0ec6b90a6760c86e2a05d7ab5072e8be1f9673
Sean <seanhan18@hotmail.com>
2022-12-03 23:54:58 -0600

    Merge branch 'Grant' of github.com:SeanHan18/Grace-Shopper into sean

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,13 +3,13 @@ const { Client } = require('pg');
 const { DATABASE_URL } = process.env;
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "ecommerce",
-  password: "password",
-  port: 5432,
-  // connectionString: DATABASE_URL,
-  // // database: "graceshopper"
+  // user: "postgres",
+  // host: "localhost",
+  // database: "ecommerce",
+  // password: "password",
+  // port: 5432,
+  connectionString: DATABASE_URL,
+  database: "graceshopper"
 })
   
 module.exports = client
